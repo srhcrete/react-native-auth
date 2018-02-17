71c863f88570b9afc599ca722642836857cf7d23
Sarah Lindmar <srhcrete@gmail.com>
2018-02-16 19:03:09 -0800

    install and config firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Firebase } from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDaqvIWwRHsPJWku2gDtTk-TvczVNgLB4w',
+      authDomain: 'auth-de879.firebaseapp.com',
+      databaseURL: 'https://auth-de879.firebaseio.com',
+      projectId: 'auth-de879',
+      storageBucket: 'auth-de879.appspot.com',
+      messagingSenderId: '489422794717'
+    });
+  }
+  
   render() {
     return (
       <View>
+      <Header headerText='Authentication' />
         <Text>bleh</Text>
       </View>
     );
