4084a1113454fcc0ff839a19ab33df794d29130b
Sarah Lindmar <srhcrete@gmail.com>
2018-02-17 20:41:05 -0800

    Adds spinner while checking the login state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDaqvIWwRHsPJWku2gDtTk-TvczVNgLB4w',
@@ -14,16 +16,50 @@ class App extends Component {
       storageBucket: 'auth-de879.appspot.com',
       messagingSenderId: '489422794717'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button>Log Out</Button>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <View style={styles.alignSpinnerStyle}><Spinner size='large' /></View>;
+    }
+
+    if (this.state.loggedIn) {
+      return (
+        <Button>
+          Log Out
+        </Button>
+      );
+    }
+    return <LoginForm />
   }
 
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
 }
+
+const styles = {
+  alignSpinnerStyle: {
+    margin: 40,
+  }
+};
 
 export default App;
